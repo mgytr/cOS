5e221364f6dbef1214bc10f11a525503d5811f7b
mgytr <email@mgyt.cf>
2023-12-02 12:04:07 +0100

    Delete unused file

--- system/usermanagment_ui.py
+++
@@ -1,5 +0,0 @@
-import json, colorama, getpass, os, hashlib
-def new():
-    pass
-
-print(input(f'{colorama.Fore.GREEN}Choose your option:\n1: New, 2: Remove, 3: Change password{colorama.Fore.RESET} '))
